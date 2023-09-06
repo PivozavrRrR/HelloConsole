1948a00978935639cf2efa72c2d7810420b3f38e
Пивозавр <da_net2019@mail.ru>
2023-09-07 01:29:25 +0300

    Findind sum of 2 numbers

--- Exm03SUM/Program.cs
+++ Exm03SUM/Program.cs
@@ -1,5 +1,12 @@
-﻿int numberA = 13;
-int numberB = 52;
+﻿int numberA = new Random().Next(1,10); // от 1 до 9 вкл
+
+Console.Write(numberA);
+
+int numberB = new Random().Next(1,10);
+
+Console.Write(numberB);
+
 int result = numberA + numberB;
+
 Console.WriteLine(result);
 
