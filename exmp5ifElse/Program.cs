a4db549ff4b654804eb7c6fee2d1efc643c84ae2
Пивозавр <da_net2019@mail.ru>
2023-09-07 23:37:07 +0300

    a?

--- exmp5ifElse/Program.cs
+++ exmp5ifElse/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите имя пользователя");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "маша")
+if(username.ToLower() == "маша") //ToLower переводит все символы в строке в нижний регистр
 {
     Console.WriteLine("ОМГ Маша привеееет!!!!!");
 }
